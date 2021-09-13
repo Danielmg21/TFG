ce002cd01d2e6e79aac5ac65c6fd269fd74638e7
Danielmg <Danielmg_21@hotmail.es>
2021-09-13 17:53:19 +0200

    Cleaning repositories

---
+++ pruebas/app/src/main/java/com/example/pruebas/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.pruebas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webview;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webview = (WebView) findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setAllowFileAccess(true);
+        webview.getSettings().setAllowFileAccessFromFileURLs(true);
+        webview.loadUrl("file:///android_asset/Project3/index.html");
+    }
+}
---
+++ pruebas/app/src/main/java/com/example/pruebas/MainScreen.java
@@ -0,0 +1,14 @@
+package com.example.pruebas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.os.Bundle;
+
+public class MainScreen extends AppCompatActivity {
+
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainscreen);
+    }
+}
